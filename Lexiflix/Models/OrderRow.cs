2938a90bb6c09cab80ad30367c47107317372752
hibaa <hibaa@SAYEDA>
2025-06-09 10:45:58 +0200

    Add ORederRow Model

---
+++ Lexiflix/Models/OrderRow.cs
@@ -0,0 +1,13 @@
+﻿namespace Lexiflix.Models
+{
+    public class OrderRow
+    {
+        public int Id { get; set; }
+        public int OrderId { get; set; }
+        public int MovieId { get; set; }
+        public Decimal Price { get; set; }
+
+        //public Order order { get; set; }
+        //public Movie movie { get; set; }
+    }
+}
