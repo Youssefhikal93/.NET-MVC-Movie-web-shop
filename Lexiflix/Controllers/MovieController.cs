ac25183956384113c638f0f3d32a59a5c31bc848
youssefHikal <youssefhikal@gmail.com>
2025-06-12 08:25:02 +0200

    Paginate admin view

--- Lexiflix/Controllers/MovieController.cs
+++ Lexiflix/Controllers/MovieController.cs
@@ -1,5 +1,7 @@
 ﻿using Lexiflix.Services;
 using Microsoft.AspNetCore.Mvc;
+using System.Drawing.Printing;
+using System.Globalization;
 
 namespace Lexiflix.Controllers
 {
@@ -16,10 +18,18 @@ namespace Lexiflix.Controllers
             var movies = _movieServices.GetAllMovies();
             return View(movies);
         }
-        public IActionResult AdminIndex()
+
+
+        [HttpGet]
+        [Route("Admin/Movie")]
+        public IActionResult AdminIndex(string searchString = "", string sortBy = "latest", int pageNumber = DefaultPage, int pageSize = DefaultPageSize)
         {
-            var movies = _movieServices.GetAllMovies();
-            return View(movies);
+            //var movies = _movieServices.GetAllMovies();
+            var movies = _movieServices.GetMovies(searchString, sortBy, pageNumber, pageSize);
+
+            ViewData["CurrentFilter"] = searchString;
+            ViewData["CurrentSort"] = sortBy;
+            ViewData["PageSize"] = pageSize; return View("AdminIndex", movies);
         }
     }
 }
