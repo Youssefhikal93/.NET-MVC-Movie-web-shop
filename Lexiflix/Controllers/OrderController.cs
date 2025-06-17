44cb1baa22ee0e90efed4718368475787865192d
youssefHikal <youssefhikal@gmail.com>
2025-06-17 14:53:38 +0200

    added add to cart button using js

--- Lexiflix/Controllers/OrderController.cs
+++ Lexiflix/Controllers/OrderController.cs
@@ -1,4 +1,310 @@
-﻿using Lexiflix.Models;
+﻿//using Lexiflix.Models;
+//using Lexiflix.Services;
+//using Microsoft.AspNetCore.Mvc;
+//using Newtonsoft.Json;
+
+//namespace Lexiflix.Controllers
+//{
+//    public class OrderController : Controller
+//    {
+//        private readonly IOrderServices _orderServices;
+//        private readonly ICustomerServices _customerServices;
+
+//        private const string CartSessionKey = "Cart";
+
+//        public OrderController(IOrderServices orderServices, ICustomerServices customerServices)
+//        {
+//            _orderServices = orderServices;
+//            _customerServices = customerServices;
+
+//        }
+//        public IActionResult Index()
+//        {
+//            var orders = _orderServices.GetAllOrders();
+//            return View(orders);
+//        }
+//        public IActionResult OrderDetail()
+//        {
+//            var orders = _orderServices.GetAllOrders();
+//            if (orders == null)
+//                return NotFound();
+//            return View(orders);
+//        }
+//        public IActionResult Create()
+//        {
+//            return View();
+//        }
+
+//        public IActionResult ViewCart()
+//        {
+//            var cartJson = HttpContext.Session.GetString("Cart") ?? "[]";
+//            var cart = JsonConvert.DeserializeObject<List<OrderRow>>(cartJson);
+//            foreach (var row in cart)
+//            {
+
+//            }
+//            return View(cart);
+
+//        }
+
+//        //[HttpPost] old sayda
+//        //public IActionResult AddToCart(int movieId, decimal price)
+//        //{
+//        //    Console.WriteLine($"DEBUG: Added movieId {movieId} with price {price} to cart");
+//        //    var cartJson = HttpContext.Session.GetString("Cart") ?? "[]";
+//        //    var cart = JsonConvert.DeserializeObject<List<OrderRow>>(cartJson)?? new List<OrderRow>();
+//        //    var existingItem = cart.FirstOrDefault(x => x.MovieId == movieId);
+
+
+//        //    if (existingItem != null)
+//        //    {
+//        //        cart.FirstOrDefault(x => x.MovieId == movieId).Quantity++;
+//        //    }
+//        //    else
+//        //    {
+//        //        cart.Add(new OrderRow { MovieId = movieId, Price = price, Quantity = 1 });
+
+//        //    }
+//        //    HttpContext.Session.SetString("Cart", JsonConvert.SerializeObject(cart));
+//        //    return RedirectToAction("ViewCart");
+
+
+//        //}
+//        [HttpPost]
+//        public async Task<IActionResult> AddToCart([FromBody] AddToCartRequest request)
+//        {
+//            var cartJson = HttpContext.Session.GetString("Cart") ?? "[]";
+//            var cart = JsonConvert.DeserializeObject<List<OrderRow>>(cartJson) ?? new List<OrderRow>();
+//            var existingItem = cart.FirstOrDefault(x => x.MovieId == request.MovieId);
+
+//            if (existingItem != null)
+//            {
+//                existingItem.Quantity++;
+//            }
+//            else
+//            {
+//                cart.Add(new OrderRow
+//                {
+//                    MovieId = request.MovieId,
+//                    Price = request.Price,
+//                    Quantity = 1
+//                });
+//            }
+
+//            HttpContext.Session.SetString("Cart", JsonConvert.SerializeObject(cart));
+
+//            // Calculate totals
+//            var totalItems = cart.Sum(item => item.Quantity);
+//            var totalPrice = cart.Sum(item => item.Price * item.Quantity);
+
+//            return Json(new
+//            {
+//                success = true,
+//                totalItems,
+//                totalPrice
+//            });
+//        }
+
+
+//        //[HttpPost]
+//        //public IActionResult RemoveFromCart(int movieId)
+//        //{
+//        //    var cartJson = HttpContext.Session.GetString("Cart") ?? "[]";
+//        //    var cart = JsonConvert.DeserializeObject<List<OrderRow>>(cartJson);
+//        //    var existingItem = cart.FirstOrDefault(x => x.MovieId == movieId);
+
+//        //    if (existingItem != null)
+//        //    {
+//        //        if (existingItem.Quantity > 1)
+//        //        {
+//        //            existingItem.Quantity--;
+//        //        }
+//        //        else
+//        //        {
+//        //            cart.Remove(existingItem);
+
+
+//        //        }
+//        //        HttpContext.Session.SetString("Cart", JsonConvert.SerializeObject(cart));
+//        //    }
+//        //    return RedirectToAction("ViewCart");
+//        //}
+
+//        [HttpPost]
+//        public IActionResult Checkout(string email, Customer customer)
+//        {
+//            var cartJson = HttpContext.Session.GetString("Cart") ?? "[]";
+//            var cart = JsonConvert.DeserializeObject<List<OrderRow>>(cartJson);
+//            if (cart.Count == 0)
+//                return RedirectToAction("ViewCart");
+
+//            var existingCustomer = _customerServices.GetCustomerByEmail(email);
+//            if (existingCustomer == null)
+//            {
+//                _customerServices.AddNewCustomer(customer);
+//                existingCustomer = customer;
+//            }
+//            var order = new Order
+//            {
+//                OrderDate = DateTime.Now,
+//                CustomerId = existingCustomer.Id,
+//                OrderRows = cart
+//            };
+//            _orderServices.CreateOrder(order);
+//            HttpContext.Session.Remove("Cart");
+//            return RedirectToAction("OrderSuccess");
+
+//        }
+//        [HttpGet]
+//        public IActionResult GetCartSummary()
+//        {
+//            var cartJson = HttpContext.Session.GetString("Cart") ?? "[]";
+//            var cart = JsonConvert.DeserializeObject<List<OrderRow>>(cartJson) ?? new List<OrderRow>();
+
+//            var totalItems = cart.Sum(item => item.Quantity);
+//            var totalPrice = cart.Sum(item => item.Price * item.Quantity);
+
+//            return Json(new { totalItems, totalPrice });
+//        }
+
+//        //[HttpPost]
+
+//        //public IActionResult AddCopy(int movieId)
+//        //{
+
+//        //    var cartJson = HttpContext.Session.GetString("Cart") ?? "[]";
+//        //    var cart = JsonConvert.DeserializeObject<List<OrderRow>>(cartJson);
+
+//        //    var existingItem = cart.FirstOrDefault(x => x.MovieId == movieId);
+//        //    if (existingItem != null)
+//        //    {
+//        //        existingItem.Quantity++; // increase copies
+//        //    }
+
+//        //    HttpContext.Session.SetString("Cart", JsonConvert.SerializeObject(cart));
+
+//        //    return RedirectToAction("ViewCart");
+//        //}
+
+
+//        public IActionResult OrderSuccess()
+//        {
+//            return View();
+//        }
+
+//        //[HttpPost]
+//        //public async Task<IActionResult> AddToCart([FromBody] AddToCartRequest request)
+//        //{
+//        //    var cartJson = HttpContext.Session.GetString("Cart") ?? "[]";
+//        //    var cart = JsonConvert.DeserializeObject<List<OrderRow>>(cartJson) ?? new List<OrderRow>();
+//        //    var existingItem = cart.FirstOrDefault(x => x.MovieId == request.MovieId);
+
+//        //    if (existingItem != null)
+//        //    {
+//        //        existingItem.Quantity++;
+//        //    }
+//        //    else
+//        //    {
+//        //        cart.Add(new OrderRow
+//        //        {
+//        //            MovieId = request.MovieId,
+//        //            Price = request.Price,
+//        //            Quantity = 1
+//        //        });
+//        //    }
+
+//        //    HttpContext.Session.SetString("Cart", JsonConvert.SerializeObject(cart));
+
+//        //    // Calculate totals
+//        //    var totalItems = cart.Sum(item => item.Quantity);
+//        //    var totalPrice = cart.Sum(item => item.Price * item.Quantity);
+//        //    var itemQuantity = cart.FirstOrDefault(x => x.MovieId == request.MovieId)?.Quantity ?? 0;
+
+//        //    return Json(new
+//        //    {
+//        //        success = true,
+//        //        totalItems,
+//        //        totalPrice,
+//        //        itemQuantity
+//        //    });
+//        //}
+
+//        //[HttpPost]
+//        //public async Task<IActionResult> AddCopy([FromBody] AddToCartRequest request)
+//        //{
+//        //    var cartJson = HttpContext.Session.GetString("Cart") ?? "[]";
+//        //    var cart = JsonConvert.DeserializeObject<List<OrderRow>>(cartJson) ?? new List<OrderRow>();
+//        //    var existingItem = cart.FirstOrDefault(x => x.MovieId == request.MovieId);
+
+//        //    if (existingItem != null)
+//        //    {
+//        //        existingItem.Quantity++;
+//        //    }
+
+//        //    HttpContext.Session.SetString("Cart", JsonConvert.SerializeObject(cart));
+
+//        //    // Calculate totals
+//        //    var totalItems = cart.Sum(item => item.Quantity);
+//        //    var totalPrice = cart.Sum(item => item.Price * item.Quantity);
+//        //    var itemQuantity = existingItem?.Quantity ?? 0;
+
+//        //    return Json(new
+//        //    {
+//        //        success = true,
+//        //        totalItems,
+//        //        totalPrice,
+//        //        itemQuantity
+//        //    });
+//        //}
+
+//        //[HttpPost]
+//        //public async Task<IActionResult> RemoveFromCart([FromBody] AddToCartRequest request)
+//        //{
+//        //    var cartJson = HttpContext.Session.GetString("Cart") ?? "[]";
+//        //    var cart = JsonConvert.DeserializeObject<List<OrderRow>>(cartJson) ?? new List<OrderRow>();
+//        //    var existingItem = cart.FirstOrDefault(x => x.MovieId == request.MovieId);
+
+//        //    if (existingItem != null)
+//        //    {
+//        //        if (existingItem.Quantity > 1)
+//        //        {
+//        //            existingItem.Quantity--;
+//        //        }
+//        //        else
+//        //        {
+//        //            cart.Remove(existingItem);
+//        //        }
+
+//        //        HttpContext.Session.SetString("Cart", JsonConvert.SerializeObject(cart));
+//        //    }
+
+//        //    // Calculate totals
+//        //    var totalItems = cart.Sum(item => item.Quantity);
+//        //    var totalPrice = cart.Sum(item => item.Price * item.Quantity);
+//        //    var itemQuantity = cart.FirstOrDefault(x => x.MovieId == request.MovieId)?.Quantity ?? 0;
+
+//        //    return Json(new
+//        //    {
+//        //        success = true,
+//        //        totalItems,
+//        //        totalPrice,
+//        //        itemQuantity
+//        //    });
+//        //}
+
+//        public class AddToCartRequest
+//        {
+//            public int MovieId { get; set; }
+//            public decimal Price { get; set; }
+//        }
+
+
+//    }
+
+
+//}
+
+using Lexiflix.Models;
 using Lexiflix.Services;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
@@ -9,20 +315,21 @@ namespace Lexiflix.Controllers
     {
         private readonly IOrderServices _orderServices;
         private readonly ICustomerServices _customerServices;
-       
+
         private const string CartSessionKey = "Cart";
 
         public OrderController(IOrderServices orderServices, ICustomerServices customerServices)
         {
             _orderServices = orderServices;
             _customerServices = customerServices;
-
         }
+
         public IActionResult Index()
         {
             var orders = _orderServices.GetAllOrders();
             return View(orders);
         }
+
         public IActionResult OrderDetail()
         {
             var orders = _orderServices.GetAllOrders();
@@ -30,6 +337,7 @@ namespace Lexiflix.Controllers
                 return NotFound();
             return View(orders);
         }
+
         public IActionResult Create()
         {
             return View();
@@ -39,59 +347,86 @@ namespace Lexiflix.Controllers
         {
             var cartJson = HttpContext.Session.GetString("Cart") ?? "[]";
             var cart = JsonConvert.DeserializeObject<List<OrderRow>>(cartJson);
-            foreach (var row in cart)
-            {
-
-            }
             return View(cart);
-
         }
 
         [HttpPost]
-        public IActionResult AddToCart(int movieId, decimal price)
+        public async Task<IActionResult> AddToCart([FromBody] AddToCartRequest request)
         {
-            Console.WriteLine($"DEBUG: Added movieId {movieId} with price {price} to cart");
             var cartJson = HttpContext.Session.GetString("Cart") ?? "[]";
-            var cart = JsonConvert.DeserializeObject<List<OrderRow>>(cartJson)?? new List<OrderRow>();
-            var existingItem = cart.FirstOrDefault(x => x.MovieId == movieId);
-
+            var cart = JsonConvert.DeserializeObject<List<OrderRow>>(cartJson) ?? new List<OrderRow>();
+            var existingItem = cart.FirstOrDefault(x => x.MovieId == request.MovieId);
 
             if (existingItem != null)
             {
-                cart.FirstOrDefault(x => x.MovieId == movieId).Quantity++;
+                existingItem.Quantity++;
             }
             else
             {
-                cart.Add(new OrderRow { MovieId = movieId, Price = price, Quantity = 1 });
-
+                cart.Add(new OrderRow
+                {
+                    MovieId = request.MovieId,
+                    Price = request.Price,
+                    Quantity = 1
+                });
             }
+
             HttpContext.Session.SetString("Cart", JsonConvert.SerializeObject(cart));
-            return RedirectToAction("ViewCart");
 
+            // Calculate totals
+            var totalItems = cart.Sum(item => item.Quantity);
+            var totalPrice = cart.Sum(item => item.Price * item.Quantity);
+            var itemQuantity = cart.FirstOrDefault(x => x.MovieId == request.MovieId)?.Quantity ?? 0;
 
+            return Json(new
+            {
+                success = true,
+                totalItems,
+                totalPrice,
+                itemQuantity
+            });
         }
+
         [HttpPost]
-        public IActionResult RemoveFromCart(int movieId)
+        public async Task<IActionResult> UpdateQuantity([FromBody] UpdateQuantityRequest request)
         {
             var cartJson = HttpContext.Session.GetString("Cart") ?? "[]";
-            var cart = JsonConvert.DeserializeObject<List<OrderRow>>(cartJson);
-            var existingItem = cart.FirstOrDefault(x => x.MovieId == movieId);
+            var cart = JsonConvert.DeserializeObject<List<OrderRow>>(cartJson) ?? new List<OrderRow>();
+            var existingItem = cart.FirstOrDefault(x => x.MovieId == request.MovieId);
 
             if (existingItem != null)
             {
-                if (existingItem.Quantity > 1)
+                if (request.Action == "increase")
                 {
-                    existingItem.Quantity--;
+                    existingItem.Quantity++;
                 }
-                else
+                else if (request.Action == "decrease")
                 {
-                    cart.Remove(existingItem);
-
-
+                    if (existingItem.Quantity > 1)
+                    {
+                        existingItem.Quantity--;
+                    }
+                    else
+                    {
+                        cart.Remove(existingItem);
+                    }
                 }
-                HttpContext.Session.SetString("Cart", JsonConvert.SerializeObject(cart));
             }
-            return RedirectToAction("ViewCart");
+
+            HttpContext.Session.SetString("Cart", JsonConvert.SerializeObject(cart));
+
+            // Calculate totals
+            var totalItems = cart.Sum(item => item.Quantity);
+            var totalPrice = cart.Sum(item => item.Price * item.Quantity);
+            var itemQuantity = cart.FirstOrDefault(x => x.MovieId == request.MovieId)?.Quantity ?? 0;
+
+            return Json(new
+            {
+                success = true,
+                totalItems,
+                totalPrice,
+                itemQuantity
+            });
         }
 
         [HttpPost]
@@ -117,56 +452,45 @@ namespace Lexiflix.Controllers
             _orderServices.CreateOrder(order);
             HttpContext.Session.Remove("Cart");
             return RedirectToAction("OrderSuccess");
-
         }
 
-        [HttpPost]
-
-        public IActionResult AddCopy(int movieId)
+        [HttpGet]
+        public IActionResult GetCartSummary()
         {
-          
             var cartJson = HttpContext.Session.GetString("Cart") ?? "[]";
-            var cart = JsonConvert.DeserializeObject<List<OrderRow>>(cartJson);
-           
-            var existingItem = cart.FirstOrDefault(x => x.MovieId == movieId);
-            if (existingItem != null)
-            {
-                existingItem.Quantity++; // increase copies
-            }
+            var cart = JsonConvert.DeserializeObject<List<OrderRow>>(cartJson) ?? new List<OrderRow>();
 
-            HttpContext.Session.SetString("Cart", JsonConvert.SerializeObject(cart));
-          
-            return RedirectToAction("ViewCart");
+            var totalItems = cart.Sum(item => item.Quantity);
+            var totalPrice = cart.Sum(item => item.Price * item.Quantity);
+
+            return Json(new { totalItems, totalPrice });
         }
-        [HttpPost]
-        //public IActionResult RemoveCopy(int movieId)
-        //{
-        //    var cartJson = HttpContext.Session.GetString("Cart") ?? "[]";
-        //    var cart = JsonConvert.DeserializeObject<List<OrderRow>>(cartJson);
-        //    var existingItem = cart.FirstOrDefault(x => x.MovieId == movieId);
-        //    if (existingItem != null)
 
-        //        if (existingItem.Quantity > 1)
-        //        {
+        [HttpGet]
+        public IActionResult GetItemQuantity(int movieId)
+        {
+            var cartJson = HttpContext.Session.GetString("Cart") ?? "[]";
+            var cart = JsonConvert.DeserializeObject<List<OrderRow>>(cartJson) ?? new List<OrderRow>();
+            var item = cart.FirstOrDefault(x => x.MovieId == movieId);
 
-        //            existingItem.Quantity--; // Reduce quantity
-        //        }
-        //        else
-        //        {
-        //            cart.Remove(existingItem); // Remove item if only one copy exists
-        //        }
-
-        //    HttpContext.Session.SetString("Cart", JsonConvert.SerializeObject(cart));
-        //    return RedirectToAction("ViewCart");
-        //}
+            return Json(new { quantity = item?.Quantity ?? 0 });
+        }
 
         public IActionResult OrderSuccess()
         {
             return View();
         }
 
+        public class AddToCartRequest
+        {
+            public int MovieId { get; set; }
+            public decimal Price { get; set; }
+        }
 
+        public class UpdateQuantityRequest
+        {
+            public int MovieId { get; set; }
+            public string Action { get; set; }
+        }
     }
-
-
 }
--- Lexiflix/wwwroot/js/site.js
+++ Lexiflix/wwwroot/js/site.js
@@ -1,4 +1,209 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿//// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+//// for details on configuring this project to bundle and minify static web assets.
 
+//// Write your JavaScript code.
+//document.addEventListener('DOMContentLoaded', function () {
+
+//    // Handle add to cart button clicks
+//    document.querySelectorAll('.add-to-cart').forEach(button => {
+//        button.addEventListener('click', async function () {
+//            const movieId = this.getAttribute('data-movie-id');
+//            const price = this.getAttribute('data-price');
+
+//            try {
+//                const response = await fetch('/Order/AddToCart', {
+//                    method: 'POST',
+//                    headers: {
+//                        'Content-Type': 'application/json',
+//                    },
+//                    body: JSON.stringify({
+//                        movieId: parseInt(movieId),
+//                        price: parseFloat(price)
+//                    })
+//                });
+
+//                if (response.ok) {
+//                    const result = await response.json();
+//                    updateCartUI(result.totalItems, result.totalPrice);
+
+//                    // Show a quick notification
+//                    const notification = document.createElement('div');
+//                    notification.className = 'position-fixed bottom-0 end-0 mb-3 me-3 p-2 bg-warning text-dark rounded shadow';
+//                    notification.style.zIndex = '1000';
+//                    notification.innerHTML = '<i class="bi bi-check-circle-fill me-1"></i> Added to cart!';
+//                    document.body.appendChild(notification);
+
+//                    setTimeout(() => {
+//                        notification.remove();
+//                    }, 2000);
+//                }
+//            } catch (error) {
+//                console.error('Error:', error);
+//            }
+//        });
+//    });
+
+//    // Function to update cart UI
+//    function updateCartUI(totalItems, totalPrice) {
+//        const cartButton = document.querySelector('.cart-button');
+//        if (cartButton) {
+//            cartButton.innerHTML = `
+//                    <i class="bi bi-cart4 me-2"></i>
+//                    ${totalItems > 0 ? `<span class="badge bg-danger rounded-pill">${totalItems}</span>` : ''}
+//                    ${totalPrice > 0 ? `<span class="ms-1">${totalPrice} Kr</span>` : ''}
+//                `;
+//        }
+//    }
+//});
+// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
 // Write your JavaScript code.
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Load initial quantities for all movie cards
+    loadMovieQuantities();
+
+    // Handle add to cart button clicks
+    document.querySelectorAll('.add-to-cart').forEach(button => {
+        button.addEventListener('click', async function () {
+            const movieId = this.getAttribute('data-movie-id');
+            const price = this.getAttribute('data-price');
+
+            try {
+                const response = await fetch('/Order/AddToCart', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({
+                        movieId: parseInt(movieId),
+                        price: parseFloat(price)
+                    })
+                });
+
+                if (response.ok) {
+                    const result = await response.json();
+                    updateCartUI(result.totalItems, result.totalPrice);
+                    updateMovieCard(movieId, result.itemQuantity);
+                    showNotification('Added to cart!');
+                }
+            } catch (error) {
+                console.error('Error:', error);
+            }
+        });
+    });
+
+    // Handle quantity increase buttons
+    document.querySelectorAll('.increase-quantity').forEach(button => {
+        button.addEventListener('click', async function () {
+            const movieId = this.getAttribute('data-movie-id');
+            await updateQuantity(movieId, 'increase');
+        });
+    });
+
+    // Handle quantity decrease buttons
+    document.querySelectorAll('.decrease-quantity').forEach(button => {
+        button.addEventListener('click', async function () {
+            const movieId = this.getAttribute('data-movie-id');
+            await updateQuantity(movieId, 'decrease');
+        });
+    });
+});
+
+async function loadMovieQuantities() {
+    const movieCards = document.querySelectorAll('.add-to-cart');
+
+    for (const button of movieCards) {
+        const movieId = button.getAttribute('data-movie-id');
+        try {
+            const response = await fetch(`/Order/GetItemQuantity?movieId=${movieId}`);
+            if (response.ok) {
+                const result = await response.json();
+                if (result.quantity > 0) {
+                    updateMovieCard(movieId, result.quantity);
+                }
+            }
+        } catch (error) {
+            console.error('Error loading quantity for movie', movieId, error);
+        }
+    }
+}
+
+async function updateQuantity(movieId, action) {
+    try {
+        const response = await fetch('/Order/UpdateQuantity', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                movieId: parseInt(movieId),
+                action: action
+            })
+        });
+
+        if (response.ok) {
+            const result = await response.json();
+            updateCartUI(result.totalItems, result.totalPrice);
+            updateMovieCard(movieId, result.itemQuantity);
+
+            if (action === 'increase') {
+                showNotification('Quantity increased!');
+            } else if (result.itemQuantity === 0) {
+                showNotification('Item removed from cart!');
+            } else {
+                showNotification('Quantity decreased!');
+            }
+        }
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+function updateMovieCard(movieId, quantity) {
+    const movieCard = document.querySelector(`[data-movie-id="${movieId}"]`).closest('.card');
+    const addButton = movieCard.querySelector('.add-to-cart');
+    const quantityControls = movieCard.querySelector('.quantity-controls');
+    const quantityInput = movieCard.querySelector('.quantity-input');
+
+    if (quantity > 0) {
+        // Hide add button, show quantity controls
+        addButton.classList.add('d-none');
+        quantityControls.classList.remove('d-none');
+        quantityInput.value = quantity;
+    } else {
+        // Show add button, hide quantity controls
+        addButton.classList.remove('d-none');
+        quantityControls.classList.add('d-none');
+        quantityInput.value = 1;
+    }
+}
+
+function updateCartUI(totalItems, totalPrice) {
+    const cartButton = document.querySelector('.cart-button');
+    if (cartButton) {
+        if (totalItems > 0) {
+            cartButton.innerHTML = `
+                <i class="bi bi-cart4 me-2"></i>
+                <span class="badge bg-danger rounded-pill me-1">${totalItems}</span>
+                <span>${totalPrice.toFixed(2)} Kr</span>
+            `;
+        } else {
+            cartButton.innerHTML = `
+                <i class="bi bi-cart4 me-2"></i>Shopping Cart
+            `;
+        }
+    }
+}
+
+function showNotification(message) {
+    const notification = document.createElement('div');
+    notification.className = 'position-fixed bottom-0 end-0 mb-3 me-3 p-2 bg-warning text-dark rounded shadow';
+    notification.style.zIndex = '1000';
+    notification.innerHTML = `<i class="bi bi-check-circle-fill me-1"></i> ${message}`;
+    document.body.appendChild(notification);
+
+    setTimeout(() => {
+        notification.remove();
+    }, 2000);
+}
